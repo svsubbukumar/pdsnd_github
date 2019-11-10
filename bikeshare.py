53490d159e89854168f991f8ae34b8be130c0ab9
Sam Subbukumar <svsubbukumar@gmail.com>
2019-11-10 13:24:50 -0800

    refactor: Added my name, date, and project title

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,11 @@ import time
 import pandas as pd
 import numpy as np
 
+#Sam Subbukumar
+#Udacity Python project
+#11/4/2019
+
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
